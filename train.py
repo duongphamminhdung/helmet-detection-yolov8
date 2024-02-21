3bc74b5d1d478f3913ea77c1643fbb4baaf08a94
Duong Pham Minh Dung <94067220+duongphamminhdung@users.noreply.github.com>
2024-02-21 14:40:54 +0700

    continue on colab

--- train.py
+++ train.py
@@ -10,60 +10,41 @@ import imgaug
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
-from copy import deepcopy
 
 from data.transform import Augmentation, BaseTransform
 
-from models.build import build_yolov2
+from ultralytics import YOLO
 
 def parse_args():
     parser = argparse.ArgumentParser(description='YOLOv2 Detection')
-    parser.add_argument('--cuda', action='store_true', default=False,
+    parser.add_argument('-p', '--path', default=None, type=str, 
+                        help='path to model')
+    parser.add_argument('--cuda', action='store_true', default=True,
                         help='use cuda.')
-    parser.add_argument('--tfboard', action='store_true', default=False,
-                        help='use tensorboard')
+    parser.add_argument('--epochs', type=int,
+                            default=100, help='Total number of training epochs. Each epoch represents a full pass over the entire dataset. Adjusting this value can affect training duration and model performance.')
     parser.add_argument('--eval_epoch', type=int,
                             default=5, help='interval between evaluations')
     parser.add_argument('--save_folder', default='weights/', type=str, 
                         help='Save folder')
     parser.add_argument('--num_workers', default=8, type=int, 
                         help='Number of workers used in dataloading')
-
     parser.add_argument('-bs', '--batch_size', default=8, type=int, 
                         help='Batch size for training')
-    parser.add_argument('-accu', '--accumulate', default=8, type=int, 
-                        help='gradient accumulate.')
-    parser.add_argument('-no_wp', '--no_warm_up', action='store_true', default=False,
-                        help='yes or no to choose using warmup strategy to train')
-    parser.add_argument('--wp_epoch', type=int, default=1,
-                        help='The upper bound of warm-up')
-    parser.add_argument('--start_epoch', type=int, default=0,
-                        help='start epoch to train')
+    parser.add_argument('--warmup_epochs', type=int, default=3,
+                        help='Number of epochs for learning rate warmup, gradually increasing the learning rate from a low value to the initial learning rate to stabilize training early on.')
     parser.add_argument('-r', '--resume', default=None, type=str, 
                         help='keep training')
-    parser.add_argument('-ms', '--multi_scale', action='store_true', default=False,
-                        help='use multi-scale trick')                  
-    parser.add_argument('--max_epoch', type=int, default=200,
-                        help='The upper bound of warm-up')
-    parser.add_argument('--lr_epoch', nargs='+', default=[100, 150], type=int,
-                        help='lr epoch to decay')
 
-    parser.add_argument('--lr', default=1e-3, type=float, 
-                        help='initial learning rate')
+    parser.add_argument('--lr0', default=1e-3, type=float, 
+                        help='Initial learning rate (i.e. SGD=1E-2, Adam=1E-3) . Adjusting this value is crucial for the optimization process, influencing how rapidly model weights are updated.')
+    parser.add_argument('--lrf', default=1e-3, type=float, 
+                        help='Final learning rate as a fraction of the initial rate = (lr0 * lrf), used in conjunction with schedulers to adjust the learning rate over time.')
     parser.add_argument('--momentum', default=0.9, type=float, 
                         help='Momentum value for optim')
-    parser.add_argument('--weight_decay', default=5e-4, type=float, 
-                        help='Weight decay for SGD')
-    parser.add_argument('--gamma', default=0.1, type=float, 
-                        help='Gamma update for SGD')
-    parser.add_argument('--seed', default=2506, type=int, 
-                        help='seed for iaa random')
-
-    parser.add_argument('-d', '--dataset', default='voc',
-                        help='voc or coco')
-    parser.add_argument('--root', default='/root/PyTorch_YOLOv2/setup',
-                        help='data root')
-
+    parser.add_argument('--weight_decay', default=5e-4, type=float)
+    parser.add_argument('--seed', default=2506, type=int)
+    parser.add_argument('--name', default=None, type=str)
     return parser.parse_args()
 
 
@@ -71,18 +52,39 @@ def train():
     args = parse_args()
     print("Setting Arguments.. : ", args)
     print("----------------------------------------------------------")
-
-def build_dataloader(args, dataset):
-    dataloader = torch.utils.data.DataLoader(
-                    dataset, 
-                    batch_size=args.batch_size, 
-                    shuffle=True, 
-                    collate_fn=detection_collate,
-                    num_workers=args.num_workers,
-                    pin_memory=True
-                    )
     
-    return dataloader
+    if args.cuda:
+        print("Using CUDA")
+        device = 0
+    model_name = os.path.split(args.path)[-1]
+    if model_name.endswith('.pt'):
+        model_name = model_name[:-3]
+    if args.seed is not None:
+        imgaug.random.RNG(args.seed)
+        random.seed(args.seed)
+        np.random.seed(args.seed)
+    else:
+        imgaug.random.RNG(0)
+        random.seed(0)
+        np.random.seed(0)
+        
+
+    model = YOLO(
+        model=args.path,
+        save=True,
+        save_period=10,
+        project='helmet-detection-'+model_name,
+        name=args.name,
+        exist_ok=True,
+        device = 0,
+        workers=args.num_workers,
+        seed=args.seed,
+        lr0=args.lr0,
+        lrf=args.lrf,
+        momentum=args.momentum,
+        weight_decay=args.weight_decay,
+        warmup_epochs=args.warmup_epochs,
+    )
 
 
 if __name__ == '__main__':
